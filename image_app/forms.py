1bc71a8718473e425ad88e29efd37ea6b9fd7310
Alex K <>
2024-11-16 15:42:54 +0300

    Apply black

--- image_app/forms.py
+++ image_app/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import UploadedImage
 
+
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = UploadedImage
-        fields = ['image']
+        fields = ["image"]
